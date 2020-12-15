99d7d20f44ac9e6ea366f8285755eac0ca96c07c
davek2000 <kavanad9@tcd.ie>
2020-12-15 22:57:59 +0000

    attempting to talk to GitHub but receiving communication error

--- GithubAPI/src/Lib.hs
+++ GithubAPI/src/Lib.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Lib
     ( someFunc
     ) where
 
+import qualified GitHub as GH
+import qualified Servant.Client as SC
+import           Network.HTTP.Client (defaultManagerSettings,newManager)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStrLn "Attempting to call GitHub"
+    testGitHubCall
+    putStrLn "finished"
+
+testGitHubCall :: IO ()
+
+testGitHubCall=
+    (SC.runClientM GH.test =<< env) >>= \case
+
+        Left err -> do
+         putStrLn $ "heuston, we have a problem: " ++ show err
+        Right res -> do
+         putStrLn $ "the votes of the github jury are " ++ show res
+
+
+    where env :: IO SC.ClientEnv
+          env = do
+            manager <- newManager defaultManagerSettings
+            return $ SC.mkClientEnv manager (SC.BaseUrl SC.Http "api.github.com" 80 "")
