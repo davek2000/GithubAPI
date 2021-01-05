11edb30c0327428183c2db520e3b3b197bd40b65
davek2000 <kavanad9@tcd.ie>
2021-01-05 20:06:38 +0000

    Horizontal bar chart, added x and y axis bar chart, retrieving data from mbostock profile and repos

--- GithubAPI/index.js
+++ GithubAPI/index.js
@@ -105,84 +105,113 @@ rect.data(dataArray)
 */
 //---------------------------------------------------------------------------------------------------------------------//
 const canvas = d3.select(".canvas");
-var svgHeight=500;
+var svgHeight=1000;
+var svgWidth=1000;
+var leftMargin=150;
 const svg1 = canvas.append("svg")
             .attr("width","100%")
-            .attr("height",100);
+            .attr("height","400");
 const svg2 = canvas.append("svg")
-                   .attr("width","100%")
+                   .attr("width",svgWidth)
                    .attr("height",svgHeight);
 
 const svg3 = canvas.append("svg")
                    .attr("width","100%")
-                   .attr("height",15);
+                   .attr("height",20);
 const repos=svg1.selectAll("text.title");
-const rect=svg2.selectAll("rect");
+var rect=svg2.selectAll("rect");
 const count=svg3.selectAll("text.count");
+
+var circleLink="https://api.github.com/repos/mbostock/";
 
 
 login="davek2000";
 password="PASSWORD";
 
-fetch("https://api.github.com/users/mbostock/repos", {
+fetch("https://api.github.com/users/torvalds/repos", {
   headers: new Headers({
     "Authorization": `Basic ${(`${login}:${password}`)}`
   }),
 }).then(response => {
   if (!response.ok) throw new Error(response.status);
-  else console.log(response);
 })
   
-d3.json("https://api.github.com/users/torvalds/repos?per_page=101", {
+d3.json("https://api.github.com/users/mbostock/repos?per_page=65", {
      headers: new Headers({
        "Authorization": `Basic ${(`${login}:${password}`)}`
      }),
    }).then(data => { 
      var stargazersCount =d3.map(data,function(d) {return d.stargazers_count;});
-     var test=d3.map(data,function(d) {return d.html_url;});
-
+          //Prints name of repos
           repos.data(data)
                .enter().append("text")
                .text(function(d) {return d.name;})
                .attr("class","value")
-               .attr("y",function(d,i) { return (30 * (Math.floor(i/3))+20); })
-               .attr("x",function(d,i) { return 300*(i%3); })
+               .attr("y",function(d,i) { return (30 * (Math.floor(i/5))+20); })
+               .attr("x",function(d,i) { return 300*(i%5); })
                .attr("fill", "black");
 
-          var y =d3.scaleLinear()
-                   .domain([0,d3.max(data,function(d) {return d.stargazers_count;})])
-                   .range([0,600])
+          
 
+          var stars = data.map(function(d) { return d.stargazers_count;});
+
+          var names = data.map(function(d) {return d.name;});
+
+          var scale = d3.scaleLinear()
+                       .domain([d3.min(stars), d3.max(stars)])
+                       .range([leftMargin, svgWidth - 100]);
+           
+         // Add scales to axis
+          var x_axis = d3.axisBottom()
+                        .scale(scale);
+
+          svg2.append("g")
+              .call(x_axis);
+                         
+          var nameScale = d3.scalePoint()
+                            .domain(names)
+                            .range([0,svgHeight+20]);
+
+          var nameAxis = d3.axisLeft()
+                           .scale(nameScale);
+
+          svg2.append("g")
+              .attr("transform", "translate(150, 2)")
+              .attr("class","axis")
+              .call(nameAxis);
+
+           var y =d3.scaleLinear()
+                    .domain([0,d3.max(data,function(d) {return d.stargazers_count;})])
+                    .range([0,svgWidth]);
+          //Draws bar chart
           rect.data(data)
                .enter().append("rect")                                  
-               .attr("fill","red")                                   
-               .attr("width",14)
-               .attr("height",function(d) {return y(d.stargazers_count);})                     // Height of the bar
-               .attr("x",function(d,i) {return i*70;})                                         // Gaps between bars
-               .attr("y",function(d) {return (svgHeight - y(d.stargazers_count));})
+               .attr("fill",function(d,i) {
+                 if(d.stargazers_count>99)
+                 {
+                    return "green";
+                 }
+                 else return "red";
+               })                                   
+               .attr("width",function(d) {console.log(d.stargazers_count); return y(d.stargazers_count);})
+               .attr("height",function(d,i) {return 6;})                     // Height of the bar
+               .attr("x",function(d,i) {return leftMargin+1;})                                         // Gaps between bars
+               .attr("y",function(d,i) {return i*16;})
                .append("title")
-               .text((item) => { return item.name;})
+               .text((item) => `Stargazer count for ${item.name} is ${item.stargazers_count}`);
+               
 
-          count.data(data)
-               .enter().append("text")
-               .text(function(d) {return d.stargazers_count;})
-               .attr("class","title")
-               .attr("y",function(d,i) { return 15; })
-               .attr("x",function(d,i) { return i*70; })
-               .attr("fill", "black");
-
+          svg2.append("g")
+              .on("mouseover",function() {console.log("Hello");});
 });
-
-d3.json("https://api.github.com/repos/torvalds/libdc-for-dirk/contributors?per_page=65", {
+circleLink+="stack/contributors?per_page=100";
+d3.json(circleLink, {
      headers: new Headers({
        "Authorization": `Basic ${(`${login}:${password}`)}`
      }),
    }).then(data2 => {
         var contributions = d3.map(data2,function(d) {return d.contributions;});
         var login = d3.map(data2,function(d) {return d.login;});
-        console.log(contributions);
-        console.log(login);
-        console.log(data2);
         
         // https://codepen.io/zakariachowdhury/pen/OWdyjq
         var text = "";
